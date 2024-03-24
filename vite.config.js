8c063dc2631e88938feec5f85edd2c130b23201b
vietthang3103 <vietthang3103@gmail.com>
2024-03-24 23:53:03 +0700

    first commit

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+// vite.config.js
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                shop: resolve(__dirname, "./shop.html"),
+                detail: resolve(__dirname, "./detail.html"),
+                cart: resolve(__dirname, "./cart.html"),
+                checkout: resolve(__dirname, "./checkout.html"),
+            },
+        },
+    },
+});
